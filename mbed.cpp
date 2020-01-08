8be9245be674406cf69eb38098a3dea9cb1ec5f7
leo <camp.leonardo@lyceeppr.fr>
2020-01-08 12:49:24 +0100

    oui

--- mbed.cpp
+++ mbed.cpp
@@ -1,6 +1,7 @@
 
 #include <string.h>
 #include <stdio.h>
+#include <iostream>
 #include <unistd.h>
 #include <fcntl.h>
 
@@ -10,8 +11,59 @@
 
 #include <errno.h>
 
-int sfd = open("/dev/serial0" , O_RDWR | O_NOCTY );
 
-if( sfd == -1 ){
 
+using namespace std;
+
+
+int main(){
+    
+    const int BAUDRATE = 115200;
+    
+    int sfd = open("/dev/serial/by-id/usb-STMicroelectronics_STM32_STLink_066BFF323338424E43102325-if02" , O_RDONLY | O_NOCTTY );// ouvrir le fichier 
+    
+    if( sfd == -1 ){ // verifier si il y a une erreur
+        printf( "ERROR %d\n" , errno);
+        printf( "%s\n" , strerror(errno) );
+        return (-1);
+    }else{
+        struct termios configTerm;
+        
+        bzero( &configTerm , sizeof(configTerm) );
+        
+        
+        
+        configTerm.c_cflag = BAUDRATE | CRTSCTS | CS8 | CLOCAL | CREAD //| IGNPAR | ICRNL;
+        
+        tcflush( sfd , TCIFLUSH );
+        
+        tcsetattr( sfd , TCSANOW , &configTerm );
+        
+        char buf[255];
+        int res;
+    
+        cout << "Port série ouvert en lecture seule" << "\n"; // port ouvert avec succes 
+        
+        while( true ){
+            res = read( sfd , buf , 255);
+            buf[res] = 0;
+            
+            //if( res != 0 ){
+                //printf( ":%s:%d\n" , buf , res);
+                cout << buf;
+            //}
+            
+            if( buf[0] == 'z') break;
+            
+        
+        }
+    }
+    
+    
+    
+    return 0;
+    
+    
 }
+
+
